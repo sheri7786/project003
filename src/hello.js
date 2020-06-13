010fab699e0d853d296e544871ba15273af56ff8
sheri7786 <66200396+sheri7786@users.noreply.github.com>
2020-06-13 13:09:58 -0400

    update

--- src/hello.js
+++ src/hello.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./Hello.css"
- function Hello({firstname}) {
+function Hello({ firstname }) {
   return (
-    <h1>Practice task</h1>
-    <br></br>
-    <p className="myname">
-      this is paragraph {firstname} 
-    </p>
+    <>
+      <h1>Practice task</h1>
+      <br></br>
+      <p className="myname">
+        this is paragraph {firstname}
+      </p>
+    </>
   )
 }
 export default Hello;
